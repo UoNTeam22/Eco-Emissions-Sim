1b26b13c7cc7e93bae003d5578cb1cec84f3244d
gingerchicken <32576907+gingerchicken@users.noreply.github.com>
2022-11-16 19:26:39 +0000

    Add express placeholder
    Run npm i express

--- back/src/index.js
+++ back/src/index.js
@@ -1,1 +1,12 @@
-// TODO everything!
+let express = require('express');
+let app = express();
+
+const PORT = 3000;
+
+app.get('/', function (req, res) {
+    res.send('Here\'s where it all begins!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Eco RESTful API bound locally on port ${PORT}!`);
+});
