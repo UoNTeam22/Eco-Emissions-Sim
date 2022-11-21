9ff326f451688c5e15d8a5868668133fc92b4707
Retr05041 <parker@cranfield.ca>
2022-11-21 16:57:46 +0000

    Create basic navbar component
    Only has title of project in left corner

---
+++ front/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+// Navbar component
+
+import React, { Component } from 'react';
+import '../styles/Navbar.css';
+
+class Navbar extends Component {
+    state = {}
+    render() {
+        return (
+            <div className="navbar">
+                <h1>Eco Emissions Sim</h1>
+                {/* <div class="navbar_logo"></div> */}
+            </div>
+
+
+        );
+    }
+}
+
+export default Navbar;
