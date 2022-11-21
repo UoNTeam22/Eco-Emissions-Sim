314a90c37c2acab32ea8a2e142782f2510b398c3
Retr05041 <parker@cranfield.ca>
2022-11-21 18:57:41 +0000

    Add Graph and Share buttons

--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -10,9 +10,15 @@ class Navbar extends Component {
             <div className="navbar">
                 <h1>Eco Emissions Sim</h1>
                 {/* <div class="navbar_logo"></div> */}
+                <div className="navbar_links">
+                    <div>
+                        <a href="#">Graphs</a>
+                    </div>
+                    <div>
+                        <a href="#">Share</a>
+                    </div>
+                </div>
             </div>
-
-
         );
     }
 }
