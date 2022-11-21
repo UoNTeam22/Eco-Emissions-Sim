fb7cd621b31a5f7c91dfc4ba7aa45178fe02ffdc
Retr05041 <parker@cranfield.ca>
2022-11-21 18:40:14 +0000

    Create basic view for map

---
+++ front/src/components/View.jsx
@@ -0,0 +1,19 @@
+// Body component
+
+import React, { Component } from 'react';
+import '../styles/View.css';
+
+class View extends Component {
+    state = {}
+    render() {
+        return (
+            <div className="view">
+                <div className="view_body">
+                    <h1>image</h1>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default View;
