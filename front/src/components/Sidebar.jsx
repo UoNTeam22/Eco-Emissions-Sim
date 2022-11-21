7a5080a3638ae5bfe81d97ecb08b03ff90c1338f
Retr05041 <parker@cranfield.ca>
2022-11-21 17:19:27 +0000

    Create basic empty sidebar
    Apply button at bottom of sidebar

---
+++ front/src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+// Sidebar component
+
+import React, { Component } from 'react';
+import '../styles/Sidebar.css';
+
+class Sidebar extends Component {
+    state = {}
+    render() {
+        return (
+            <div className="sidebar">
+                <div className="apply_button">
+                    <h1>Apply</h1>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default Sidebar;
