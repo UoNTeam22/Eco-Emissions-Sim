7c4c62eccca3638c02404930b9cb19d8178b171d
Retr05041 <parker@cranfield.ca>
2022-11-21 17:19:43 +0000

    Add Sidebar to App component

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
       {/* Navbar */}
       <Navbar />
+      {/* Sidebar */}
+      <Sidebar />
     </div>
   );
 }
