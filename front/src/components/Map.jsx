35b8e0dfc1f79eb9cc8311a34b2552d8e617a4f5
Kesar Valera <116122564+KesarEra@users.noreply.github.com>
2022-11-27 22:33:44 +0000

    Add countries geojson file
    Tried to show the geojson ma

--- front/src/components/Map.jsx
+++ front/src/components/Map.jsx
@@ -1,12 +1,20 @@
 // Map component
 import React, { Component } from 'react';
+import { MapContainer, GeoJSON } from "react-leaflet";
+import mapData from "./../data/countries.json";
+import "leaflet/dist/leaflet.css";
 
 class Map extends Component {
     state = {}
     render() {
         return (
             <React.Fragment>
-                <h1>My Map</h1>
+                <h1>Map</h1>
+                {/* Do not understand why this is not working!
+                <MapContainer zoom={2} center={[20, 100]}>
+                    <GeoJSON data={mapData.features} />
+                </MapContainer>
+                */}
             </React.Fragment>
         );
     }
