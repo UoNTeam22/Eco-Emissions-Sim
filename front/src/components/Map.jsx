2a7b2ef2c4db76e2fe4a939936a7e7d5cf6afa06
Kesar Valera <116122564+KesarEra@users.noreply.github.com>
2022-11-28 22:19:10 +0000

    Display geojson map in the view section
    Also added Map.css in styles folder

--- front/src/components/Map.jsx
+++ front/src/components/Map.jsx
@@ -1,17 +1,35 @@
 // Map component
 import React, { Component } from 'react';
+import { MapContainer, GeoJSON } from "react-leaflet";
+import mapData from "./../data/countries.json";
+import "leaflet/dist/leaflet.css";
+import "./../styles/Map.css";
 
 class Map extends Component {
     state = {}
+
+    countryStyle = {
+        fillColor: "red",
+        fillOpacity: 1,
+        color: "black",
+        weight: 1,
+    };
+
+    onEachFeature = (country, layer) => {
+        const countryName = country.properties.ADMIN;
+        layer.bindPopup(countryName);
+    }
+
     render() {
         return (
             <React.Fragment>
-                <h1>Map</h1>
-                {/* Do not understand why this is not working!
-                <MapContainer zoom={2} center={[20, 100]}>
-                    <GeoJSON data={mapData.features} />
+                <MapContainer style={{height: "80vh", width: "160vh"}} zoom={2} center={[0,0]}>
+                    <GeoJSON
+                        style={this.countryStyle}
+                        data={mapData.features}
+                        onEachFeature={this.onEachFeature}
+                    />
                 </MapContainer>
-                */}
             </React.Fragment>
         );
     }
