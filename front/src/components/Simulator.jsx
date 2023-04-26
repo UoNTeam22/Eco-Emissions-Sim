4c233517e3ee72ffd17598add7429d7fe08fcd7c
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2023-04-26 22:26:45 +0000

    Rename variables
    Fix documentation
    Remove '.'

--- front/src/components/Simulator.jsx
+++ front/src/components/Simulator.jsx
@@ -6,20 +6,25 @@ import View from "./View.jsx";
 import TemperatureDataFactory from "./TemperatureDataFactory.js";
 
 export default function Simulator() {
-    // Create states for factor sliders.
-    const [slider1, setSlider1] = useState(0);
-    const [slider2, setSlider2] = useState(0);
-    // Create state for timescale slider.
+    // Create states for factor sliders
+
+    // Fossil fuels slider
+    const [fossilFuelsSlider, setFossilFuelsSlider] = useState(0);
+    // Vegitarianism slider
+    const [vegitarianismSlider, setVegitarianismSlider] = useState(0);
+
+    // Create state for timescale slider (i.e., years)
     const [year, setYear] = useState(2023);
-    // Create state for map data.
+
+    // Create state for map data
     const [countriesData, setCountries] = useState([]);
 
-    // Calling useEffect function on page load to have data in map by default.
+    // Calling useEffect function on page load to have data in map by default
     useEffect(() => {
         callCountriesData();
     }, []);
 
-    // Calling service class to get map results for selected year.
+    // Calling service class to get map results for selected year
     function callCountriesData() {
         setCountries([]);
         const loadTemperature = new TemperatureDataFactory();
@@ -28,30 +33,37 @@ export default function Simulator() {
         }, year);
     }
 
-    // Creating state variables to lower prop count.
-    const slider1State = {
-        setValue: slider1,
-        setFunction: setSlider1,
+    // Creating state variables to lower prop count
+
+    // Fossil fuels slider state
+    const fossilFuelsSliderState = {
+        setValue: fossilFuelsSlider,
+        setFunction: setFossilFuelsSlider,
     };
 
-    const slider2State = {
-        setValue: slider2,
-        setFunction: setSlider2,
+    // Vegitarianism slider state.
+    const vegitarianismSliderState = {
+        setValue: vegitarianismSlider,
+        setFunction: setVegitarianismSlider,
     };
 
+    // Create the year state to allow changing of year (as a slider)
     const yearState = {
         setValue: year,
         setFunction: setYear,
     };
 
-    // Storing states for all sliders in a list for easy access.
+    // Storing states for all sliders in a list for easy access in other components
     const allSliderStates = [
-        slider1State,
-        slider2State,
+        // Variables sliders
+        fossilFuelsSliderState,
+        vegitarianismSliderState,
+
+        // Year slider
         yearState,
     ];
 
-    // Returing all components of website.
+    // Returing all components of the simulator
     return (
         <>
             <Navbar />
