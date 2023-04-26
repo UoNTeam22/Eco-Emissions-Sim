8b727ae47db138cd5156aa70fc5e4dc34e31bbe4
psyme4 <psyme4@nottingham.ac.uk>
2023-04-26 18:53:09 +0100

    fix map rendering

--- front/src/components/TemperatureMap.jsx
+++ front/src/components/TemperatureMap.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Map from "./Map";
 import ColorKey from "./ColorKey";
 import KeyItems from "./Keys";
 
 function TemperatureMap({countries}) {
-
+  
     return (
+        countries.length === 0? <div><p>click the apply button to start</p></div>:
         <div>
             <Map countries={countries} />
             <ColorKey keyItems={[...KeyItems]} />
